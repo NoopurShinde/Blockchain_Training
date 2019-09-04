f84758e251bf152e7ee927be918c5ead09e265c8
NoopurShinde <54314296+NoopurShinde@users.noreply.github.com>
2019-09-04 16:53:01 +0530

    Create truffle-config.js

---
+++ TestCases/truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rpc: {
+  host:"10.0.12.94",
+  port:8545
+  },
+  networks: {
+  development: {
+  host: "10.0.12.94", 
+  port: 8545,
+  network_id: "*",
+  from: "0x936494d02fffa96077992de944200d2f31a68748",
+  gas: 20000000
+  }
+  }
+  };
